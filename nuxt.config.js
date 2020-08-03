44495857e330872a142d830d34dd3a9dae1e0170
Lukas Kirylak <lukasz.kirylak@indevops.com>
2020-08-03 18:09:58 +0000

    Add GH pages deployment script

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,5 +55,8 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {}
+  build: {},
+  router: {
+    base: '/blockchain-examples/'
+  }
 };
